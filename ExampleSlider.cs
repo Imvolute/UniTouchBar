66f4fb1a30d5e4606c15d51272efa0c0205f51f3
Bartosz Swiety <>
2019-05-22 14:01:57 +0200

    Names names

--- ExampleSlider.cs
+++ ExampleSlider.cs
@@ -1,20 +1,20 @@
 ﻿/*
- * 
- * 
- * UniTouchBar  - SettingsWindow  + Test Script
- * 
- * 
+ *
+ *
+ * UniTouchBar  - Slider Example  + Test Script
+ * Put me to Editor Directory
+ *
  * API:
  * http://imvolute.com/unitouchbar/api
- * 
+ *
  * GIT:
  * http://github.com/Imvolute/UniTouchBar
- * 
+ *
  * CONTACT:
  * unitouchbar@imvolute.com
- * 
- * 
- * 
+ *
+ *
+ *
  * */
 
 using System.Collections;
@@ -26,11 +26,9 @@ using UnityEngine;
 using Uni;
 
 [InitializeOnLoad]
-public class ExampleSliderTouchBar
-{
+public class ExampleSliderTouchBar {
 
-    static ExampleSliderTouchBar()
-    {
+    static ExampleSliderTouchBar () {
         //Manager will tell us when TouchBar is ready for us.
         TouchBar.Manager.OnReady += Manager_OnReady;
     }
@@ -38,67 +36,59 @@ public class ExampleSliderTouchBar
     private static TouchBar.Group sliderGroup;
     private static TouchBar.Button showSliderButton;
 
-
-    static void Manager_OnReady()
-    {
+    static void Manager_OnReady () {
         //Ready? We don't need it anymore. - keep it fast!
         TouchBar.Manager.OnReady -= Manager_OnReady;
 
         //--------------------1.GROUP----------------------------
         //--Everything is allways stored in groups.
-        sliderGroup = new TouchBar.Group("sliderGroup", 2, () =>
-        {
-            TouchBar.Log("Sliders Touchbar Loaded - you can remove it)");
+        sliderGroup = new TouchBar.Group ("sliderGroup", 2, () => {
+            TouchBar.Log ("Sliders Touchbar Loaded - you can remove it)");
         });
 
-
         //--------------------2.Items-----------------------------
-        showSliderButton = sliderGroup.AddTextButton("slidersButton", "show", () =>
-        {
+        showSliderButton = sliderGroup.AddTextButton ("slidersButton", "show", () => {
             //Show group of sliders.
-            DynamicGroup();
+            DynamicGroup ();
         });
 
         //--------------------------3.SHOW-------------------------
-        TouchBar.AddGroup(sliderGroup);
+        TouchBar.AddGroup (sliderGroup);
         //--Let's show the touchbar on all windows! for specyfic check Uni.TouchBar.Windows or string (basing on what EditorWindow..titleContent.text says)
-        sliderGroup.ShowOnWindow(Uni.TouchBar.Windows.ALL);
+        sliderGroup.ShowOnWindow (Uni.TouchBar.Windows.ALL);
         //--------------------That's it!----------------------------
 
     }
     private static TouchBar.Group moreGroup;
     private static TouchBar.Slider slider;
-    static void DynamicGroup()
-    {
+    static void DynamicGroup () {
 
         //No need to init it double times
-        if (moreGroup == null)
-        {
-            moreGroup = new TouchBar.Group("slidersGroup", 6);
+        if (moreGroup == null) {
+            moreGroup = new TouchBar.Group ("slidersGroup", 6);
 
-            slider = moreGroup.AddSlider("slider", 0, 10, 3, (double value) =>
-            {
-                Debug.Log("On Slider change [value:" + value.ToString() + "]");
+            slider = moreGroup.AddSlider ("slider", 0, 10, 3, (double value) => {
+                Debug.Log ("On Slider change [value:" + value.ToString () + "]");
             });
 
-
-
             //Close group button
-            moreGroup.AddImageButton("close", "/Editor/UniTouchBar/Icons/close.png", "", () =>
-            {
+            moreGroup.AddImageButton ("close", "/Editor/UniTouchBar/Icons/close.png", "", () => {
                 if (moreGroup != null) //Shit happens - shouldn't but in case of!.
-                    moreGroup.Hide();
+                    moreGroup.Hide ();
             });
 
             //Let's add and show!
-            TouchBar.AddGroup(moreGroup);
+            TouchBar.AddGroup (moreGroup);
             //We can hide buttons
-            moreGroup.OnHidden += () => { showSliderButton.Show(); };
-            moreGroup.OnShow += () => { showSliderButton.Hide(); };
+            moreGroup.OnHidden += () => {
+                showSliderButton.Show ();
+            };
+            moreGroup.OnShow += () => {
+                showSliderButton.Hide ();
+            };
         }
 
-        moreGroup.Show();
+        moreGroup.Show ();
     }
-
 
 }
